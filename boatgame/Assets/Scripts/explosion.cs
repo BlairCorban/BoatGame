f5c96e6ed6c913036a57ebfb25d309a9b77f3076
DESKTOP-4JI590C\The Baboon <blaircorban@hotmail.com>
2018-04-07 12:36:59 +1200

    initial commit

---
+++ boatgame/Assets/Scripts/explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour {
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            rb.AddExplosionForce(9000,this.transform.position, 1, 1);
+        }
+	}
+}
