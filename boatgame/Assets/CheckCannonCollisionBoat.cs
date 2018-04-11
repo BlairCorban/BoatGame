9c4c3e3db6e385ad3af5fa3e4f214b32eec0dc07
BlairCorban <blaircorban@hotmail.com>
2018-04-11 13:39:30 +1200

    bundy explosion for riceboat

---
+++ boatgame/Assets/CheckCannonCollisionBoat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCannonCollisionBoat : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "cannonball")
+        {
+            boatexplosion.isExplode = true;
+        }
+
+    }
+}
---
+++ boatgame/Assets/boatexplosion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boatexplosion : MonoBehaviour {
+    public Rigidbody sack1;
+    public Rigidbody sack2;
+    public Rigidbody sack3;
+    public GameObject flameprefab;
+    public static bool isExplode;
+	// Use this for initialization
+	void Start () {
+        isExplode = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isExplode)
+        {
+            sack1.AddExplosionForce(900, sack1.transform.position, 1);
+            sack2.AddExplosionForce(900, sack1.transform.position, 1);
+            sack3.AddExplosionForce(900, sack1.transform.position, 1);
+            GameObject flame = Instantiate(flameprefab,transform.position, flameprefab.transform.rotation);
+            flame.transform.parent = gameObject.transform;
+            flame.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            isExplode = false;
+        }
+	}
+    
+}
