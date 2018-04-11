e6bc3483b2a5cdfef4fabaa56fdc87042c2debf8
BlairCorban <blaircorban@hotmail.com>
2018-04-11 17:12:44 +1200

    added artists things and moved boats

--- boatgame/Assets/cannonballcontroller.cs
+++ boatgame/Assets/cannonballcontroller.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class cannonballcontroller : MonoBehaviour {
     public float speed;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         transform.Translate(Vector3.back * speed * Time.deltaTime);
-	}
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "player1")
+        {
+            Debug.Log("player 1 was hit");
+        }
+        if(other.gameObject.tag == "player2")
+        {
+            Debug.Log("player 2 was hit");
+        }
+    }
 }
